b32d74921126d49cd191b06e5173b6a186ef9463
Timo <developer.timo@gmail.com>
2022-11-03 16:46:47 +0900

    [3주차 - Stack] 괄호 맞추기 코드

---
+++ w3_stack/TimoParenthesisStringTest.java
@@ -0,0 +1,35 @@
+package week03;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Stack;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class TimoParenthesisStringTest {
+
+    @Test
+    public void test() {
+        assertThat(solution("(())())")).isEqualTo("NO");
+        assertThat(solution("(((()())()")).isEqualTo("NO");
+        assertThat(solution("(()())((()))")).isEqualTo("YES");
+        assertThat(solution("((()()(()))(((())))()")).isEqualTo("NO");
+        assertThat(solution("()()()()(()()())()")).isEqualTo("YES");
+        assertThat(solution("(()((())()(")).isEqualTo("NO");
+    }
+
+    private String solution(String input) {
+        Stack<Character> stack = new Stack<>();
+        for (char ch : input.toCharArray()) {
+            if (ch == '(') {
+                stack.push(ch);
+            } else {
+                if (stack.empty()) {
+                    return "NO";
+                }
+                stack.pop();
+            }
+        }
+        return stack.isEmpty() ? "YES" : "NO";
+    }
+}
